4c2fee35e5967f614ed8eaa576e2f360f6d9a76d
MX1D <m01064319441@gmail.com>
2022-03-17 15:25:01 +0200

    Added networth to gjoinstats

--- bot/events/gJoinStats.js
+++ bot/events/gJoinStats.js
@@ -1,6 +1,7 @@
 import { getMojangData, getSkyCryptProfile } from "../../utils/apiUtils.js";
 import { statsPhoto } from "../../utils/canvasUtils.js";
 import schema from "../../models/schema.js";
+import axios from "axios";
 
 export default {
     excute: async function (bot, text, client) {
@@ -19,6 +20,8 @@ export default {
         }
 
         const skycrypt = await getSkyCryptProfile(username).catch(e => console.log(e));
+        const networth = await axios.post("https://maro.skybrokers.xyz/api/networth/categories", { data: skycrypt.raw }).catch(e => console.log(e));
+        const total = networth.data.data.networth ? networth.data.data.networth.toLocaleString("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 0 }) : "$0";
         const skillAvg = skycrypt?.data?.average_level_no_progress || 0;
         const cataLvl = skycrypt?.data?.dungeons?.catacombs?.level?.level || 0;
         const senitherWeight = skycrypt?.data?.weight?.senither?.overall || 0;
@@ -30,9 +33,8 @@ export default {
         const totalSlayer = skycrypt?.data?.slayer_xp?.toLocaleString('en-US') || 0;
         const totalSkill = Math.round(skycrypt?.data?.total_skill_xp).toLocaleString('en-US') || 0;
         const hotmLvl = skycrypt?.data?.mining?.core?.tier?.level || 0;
-        const purse = skycrypt?.data?.purse?.toLocaleString("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 0 }) || 0;
 
-        const info = [{ title: "Skill average:", description: skillAvg }, { title: "Catacombs level:", description: cataLvl }, { title: "Senither/Lily Weight:", description: `${ Math.round(senitherWeight) }/${ Math.round(lilyWeight) }` }, { title: "Slayers:", description: `${ zombie }/${ spider }/${ wolf }/${ enderman }` }, { title: "Slayer exp:", description: totalSlayer }, { title: "Skills exp:", description: totalSkill }, { title: "HOTM:", description: hotmLvl }, { title: "Purse:", description: purse }];
+        const info = [{ title: "Skill average:", description: skillAvg }, { title: "Catacombs level:", description: cataLvl }, { title: "Senither/Lily Weight:", description: `${ Math.round(senitherWeight) }/${ Math.round(lilyWeight) }` }, { title: "Slayers:", description: `${ zombie }/${ spider }/${ wolf }/${ enderman }` }, { title: "Slayer exp:", description: totalSlayer }, { title: "Skills exp:", description: totalSkill }, { title: "HOTM:", description: hotmLvl }, { title: "Networth:", description: total }];
         statsPhoto(username, info, mojang.id, client);
     }
 };
