0ef3d79d9b651640f6487438e3c02003f0265353
MX1D <m01064319441@gmail.com>
2022-03-17 19:04:54 +0200

    lowered anti spam cooldown

--- bot/events/antiSameMsg.js
+++ bot/events/antiSameMsg.js
@@ -1,7 +1,7 @@
 export default {
     excute: async function (bot, text) {
         if (text.startsWith("You cannot say the same message twice!"))
-            setTimeout(() => { bot.say(`${ bot.lastMessage } ///////////`); }, 2000);
+            setTimeout(() => { bot.say(`${ bot.lastMessage } ///////////`); }, 1100);
         // Qulaity anti spam ik ik
     }
 };
